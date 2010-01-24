11e5ab51d29e5f63ea3e40b1261b82500af42a84
Karl Krukow <karl.krukow@gmail.com>
2010-01-24 15:07:24 +0100

    made more robust (for-in out, check existence of types, jslinted). Updated properties of global constructors

--- VersionManager.js
+++ VersionManager.js
@@ -34,153 +34,197 @@
  * @version 0.1 (alpha)
  */
 var VersionManager = (function() {
-		/**
-		 * Crockford/Cornford/Lasse Reichstein Nielsen's object function
-		 * @see http://groups.google.com/group/comp.lang.javascript/msg/e04726a66face2a2
-		 * @param {Object} origin
-		 * @return object with origin as its prototype
-		 */
-		var object = (function() {
-			function F() {}
-			return function(o) {
-				F.prototype = o;
-				return new F();
-			};
-		})();
+	/**
+	 * Crockford/Cornford/Lasse Reichstein Nielsen's object function
+	 * 
+	 * @see http://groups.google.com/group/comp.lang.javascript/msg/e04726a66face2a2
+	 * @param {Object}
+	 *            origin
+	 * @return object with origin as its prototype
+	 */
+	var object = (function() {
+		function F() {
+		}
+		return function(o) {
+			F.prototype = o;
+			return new F();
+		};
+	})();
 
-		/**
-		 * create a proxyConstructor of a given ConstructorFn type
-		 * @param ConstructorFnName
-		 * @param ConstructorFn
-		 * @return
-		 */
-		var proxyConstructor = function(ConstructorFnName, ConstructorFn) {
-			function F() {
-				if (this instanceof F && this.constructor===ConstructorFn) {
-					if (arguments.length == 0)
-						return new ConstructorFn();
-					if (arguments.length == 1) 
-						return new ConstructorFn(arguments[0]);
-					if (arguments.length == 2) 
-						return new ConstructorFn(arguments[0], arguments[1]);
-					if (arguments.length == 3) 
-						return new ConstructorFn(arguments[0], arguments[1], arguments[2]);
-					if (arguments.length == 4) 
-						return new ConstructorFn(arguments[0], arguments[1], arguments[2], arguments[3]);
-					if (arguments.length == 5) 
-						return new ConstructorFn(arguments[0], arguments[1], arguments[2], arguments[3],arguments[4]);
-					//... more if needed..
-				} else {
-					return ConstructorFn.apply(this, arguments);
+	/**
+	 * create a proxyConstructor of a given ConstructorFn type
+	 * 
+	 * @param ConstructorFnName
+	 * @param ConstructorFn
+	 * @return
+	 */
+	var proxyConstructor = function(ConstructorFnName, ConstructorFn) {
+		function F() {
+			if (this instanceof F) {
+				if (!arguments.length) {
+					return new ConstructorFn();
 				}
+				if (arguments.length == 1) {
+					return new ConstructorFn(arguments[0]);
+				}
+				if (arguments.length == 2) {
+					return new ConstructorFn(arguments[0], arguments[1]);
+				}
+				if (arguments.length == 3) {
+					return new ConstructorFn(arguments[0], arguments[1],
+							arguments[2]);
+				}
+				if (arguments.length == 4) {
+					return new ConstructorFn(arguments[0], arguments[1],
+							arguments[2], arguments[3]);
+				}
+				if (arguments.length == 5) {
+					return new ConstructorFn(arguments[0], arguments[1],
+							arguments[2], arguments[3], arguments[4]);
+				}
+				// ... more if needed..
+			} else {
+				return ConstructorFn.apply(this, arguments);
 			}
-            
-			F.prototype = F._proxyprototype = object(ConstructorFn.prototype);
-			
-			var name, consProtoSig = prototypeSignatures[ConstructorFnName];
-			for (p in consProtoSig) {
-				name = consProtoSig[p];
+		}
+
+		F.prototype = F._proxyprototype = object(ConstructorFn.prototype);
+
+		var name, sig = globalsSignatures[ConstructorFnName], i;
+		if (sig && sig.length) {
+			i = sig.length;
+			while (--i >= 0) {
+				name = sig[i];
 				F[name] = ConstructorFn[name];
 			}
-			return F;
-		};
-		
-		var proxyGlobalProperties = function(tgt, props) {
-			if (!props || !props.length > 0) {return tgt;}
-			var i = props.length;
-			while (--i >= 0) tgt[props[i]] = null;
+		}
+		return F;
+	};
+
+	var proxyGlobalProperties = function(tgt, props) {
+		var i;
+		if (props && props.length > 0) {
+			i = props.length;
+			while (--i >= 0) {
+				tgt[props[i]] = null;
+			}
 			return tgt;
-		};
-		
-		var createDelegateToVersionFn = function(ConstructorFnName, property) {
-			return function() {
-				var version = VersionManager.currentVersion; 
-				if (!version) {
-					throw new "attempt call to delegate with no version defined (see documentation)";
-				}
-				var target = version[ConstructorFnName]._proxyprototype[property];
-				if (target) {
-					return target.apply(this, arguments);
-				}
-				throw "property: "+property+" is not defined on "+ConstructorFnName+".prototype in version [" + VersionManager.currentVersion.version + "]";
-			};
-		};
-		var definePrototypeExtensions = function(ConstructorFnName, ConstructorFn, props) {
-			if (!props || props.length <= 0) return;
-			var realProto = ConstructorFn.prototype,
-				prop,
-				i = props.length;
-	        while (--i >= 0) {
-	        	prop = props[i];
-	        	realProto[prop] = createDelegateToVersionFn(ConstructorFnName, prop);
-	     	}
-		};
+		}
+		return tgt;
+	};
 
-		var globalObjectProperties = {
-			//Constructors
-			Object: Object,
-			Function: Function,
-			Array: Array,
-			String: String,
-			Boolean: Boolean,
-			Number: Number,
-			Date: Date,
-			RegExp: RegExp,
-			Error: Error,
-			EvalError: EvalError,
-			RangeError: RangeError,
-			ReferenceError: ReferenceError,
-			SyntaxError: SyntaxError,
-			TypeError: TypeError,
-			URIError:URIError,
-			//Other
-			Math: Math,
-			JSON: JSON
+	var createDelegateToVersionFn = function(ConstructorFnName, property) {
+		return function() {
+			var version = VersionManager.currentVersion;
+			if (!version) {
+				throw "attempt call to delegate with no version defined (see documentation)";
+			}
+			var target = version[ConstructorFnName]._proxyprototype[property];
+			if (target) {
+				return target.apply(this, arguments);
+			}
+			throw "property: " + property + " is not defined on "+ ConstructorFnName + ".prototype in version [" + VersionManager.currentVersion.version + "]";
 		};
+	};
+	var definePrototypeExtensions = function(ConstructorFnName, ConstructorFn, props) {
+		var realProto, prop, i;
+		if (props && props.length > 0) {
+			realProto = ConstructorFn.prototype;
+			i = props.length;
+			while (--i >= 0) {
+				prop = props[i];
+				realProto[prop] = createDelegateToVersionFn(ConstructorFnName,
+						prop);
+			}
+		}
+	};
+	
+	var globalObject = (function(){return this;})();
 
-		//Not updated yet... todo
-		var prototypeSignatures = {
-			Array: ['concat', 'join', 'pop', 'push', 'reverse', 'shift', 'slice', 'sort', 'splice', 'toSource', 'toString', 'unshift', 'valueOf'],
-			Boolean: ['toSource', 'toString', 'valueOf'],
-			Date: ['Date', 'getDate', 'getDay', 'getFullYear', 'getHours', 'getMilliseconds', 'getMinutes', 'getMonth', 'getSeconds', 'getTime', 'getTimezoneOffset', 'getUTCDate', 'getUTCDay', 'getUTCMonth', 'getUTCFullYear', 'getUTCHours', 'getUTCMinutes', 'getUTCSeconds', 'getUTCMilliseconds', 'getYear', 'parse', 'setDate', 'setFullYear', 'setHours', 'setMilliseconds', 'setMinutes', 'setMonth', 'setSeconds', 'setTime', 'setUTCDate', 'setUTCMonth', 'setUTCFullYear', 'setUTCHours', 'setUTCMinutes', 'setUTCSeconds', 'setUTCMilliseconds', 'setYear', 'toDateString', 'toGMTString', 'toLocaleDateString', 'toLocaleTimeString', 'toLocaleString', 'toSource', 'toString', 'toTimeString', 'toUTCString', 'UTC', 'valueOf'],
-			Math: ['abs', 'acos', 'asin', 'atan', 'atan2', 'ceil', 'cos', 'exp', 'floor', 'log', 'max', 'min', 'pow', 'random', 'round', 'sin', 'sqrt', 'tan', 'toSource', 'valueOf', 'E', 'LN2', 'LN10', 'LOG2E', 'LOG10E', 'PI', 'SQRT1_2', 'SQRT2'],
-			Number: ['toExponential', 'toFixed', 'toPrecision', 'toString', 'valueOf', 'NaN'],
-			String: ['anchor', 'big', 'blink', 'bold', 'charAt', 'charCodeAt', 'concat', 'fixed', 'fontcolor', 'fontsize', 'fromCharCode', 'indexOf', 'italics', 'lastIndexOf', 'link', 'match', 'replace', 'search', 'slice', 'small', 'split', 'strike', 'sub', 'substr', 'substring', 'sup', 'toLowerCase', 'toUpperCase', 'toSource', 'valueOf']
-		};
+	var globalObjectProperties = [
+	        "Object",
+	        "Function",
+	        "Array",
+	        "String",
+	        "Boolean",
+	        "Number",
+	        "Date",
+	        "RegExp",
+	        "Error",
+	        "EvalError",
+	        "RangeError",
+	        "ReferenceError",
+	        "SyntaxError",
+	        "TypeError",
+	        "URIError",
+	        "Math",
+	        "JSON"
+	];
+	
 
-		return {
-			version : function(v) {
-				if (!this.sandboxes[v]) {throw "version: "+v+" is not defined.";}
-				return this.currentVersion = this.sandboxes[v];
-			},
-			define: function(spec) {
-				if (!spec || !spec.version) {throw "bad specification: define must be called with a correct specification object (see documentation)";}
-				var sig = proxyGlobalProperties({}, spec.global || []),
-					p, spec_p, Cons;
-				for (p in globalObjectProperties) {
-					Cons = globalObjectProperties[p];
-					sig[p] = sig[p] || proxyConstructor(p, Cons);
-					spec_p = spec[p];
-					if (spec_p) {
-						definePrototypeExtensions(p, Cons, spec_p);
-					} 
-				}
-				this.sandboxes[spec.version] = sig;
-				return this.version(spec.version); 
-			},
-			sandboxes: {},
-			currentVersion : null,
-			clear: function() {
-				this.currentVersion = null;
-			},
-			complete: function(v) {
-					if (!this.sandboxes[v]) {throw "cannot complete: version '"+v+"' is not defined.";}
-					var sig = this.sandboxes[v];
-					for (var p in globalObjectProperties) {
-						sig[p].prototype = globalObjectProperties[p].prototype;
+	var globalsSignatures = {
+		Object: ['getPrototypeOf','getOwnPropertyDescriptor','getOwnPropertyNames','create',
+		         'defineProperty','defineProperties','seal','freeze','preventExtensions',
+		         'isSealed','isFrozen','isExtensible','keys'],
+        Function: ['length'],
+        Array : [ 'isArray'],
+		String : [ 'fromCharCode'],
+		Number : [ 'MAX_VALUE', 'MIN_VALUE', 'NaN', 'NEGATIVE_INFINITY',
+					'POSITIVE_INFINITY'],
+		Math : [ 'abs', 'acos', 'asin', 'atan', 'atan2', 'ceil', 'cos', 'exp',
+				'floor', 'log', 'max', 'min', 'pow', 'random', 'round', 'sin',
+				'sqrt', 'tan', 'E', 'LN2', 'LN10',
+				'LOG2E', 'LOG10E', 'PI', 'SQRT1_2', 'SQRT2' ],
+		Date : [ 'parse ','UTC','now'],
+		JSON: ['parse', 'stringify']
+	};
+
+	return {
+		version : function(v) {
+			if (!this.sandboxes[v]) {
+				throw "version: " + v + " is not defined.";
+			}
+			return (this.currentVersion = this.sandboxes[v]);
+		},
+		define : function(spec) {
+			if (!spec || !spec.version) {
+				throw "bad specification: define must be called with a correct specification object (see documentation)";
+			}
+			var sig = proxyGlobalProperties( {}, spec.global || []), 
+				i = globalObjectProperties.length, name, specForName, Cons;
+			while (--i >= 0) {
+				name = globalObjectProperties[i];
+				Cons = globalObject[name];
+				if (Cons) {
+					sig[name] = sig[name] || proxyConstructor(name, Cons);
+					specForName = spec[name];
+					if (specForName) {
+						definePrototypeExtensions(name, Cons, specForName);
 					}
-			},
-			globalObjectProperties: globalObjectProperties,
-			prototypeSignatures: prototypeSignatures
-		};
+				}
+			}
+			this.sandboxes[spec.version] = sig;
+			return this.version(spec.version);
+		},
+		sandboxes : {},
+		currentVersion : null,
+		clear : function() {
+			this.currentVersion = null;
+		},
+		complete : function(v) {
+			if (!this.sandboxes[v]) {
+				throw "cannot complete: version '" + v + "' is not defined.";
+			}
+			var sig = this.sandboxes[v],
+				i = globalObjectProperties.length, name;
+			while (--i >= 0) {
+				name = globalObjectProperties[i];
+				if (sig[name]) {
+					sig[name].prototype = globalObject[name].prototype;
+				}
+			}
+		},
+		globalObjectProperties : globalObjectProperties,
+		globalsSignatures : globalsSignatures
+	};
 })();
